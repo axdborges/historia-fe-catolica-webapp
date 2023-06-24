6d44684b35a910ed0c125adb54fb2a1bfbb82b07
axdborges <axdborges@outlook.com>
2023-06-24 16:12:41 -0300

    Feat: section endorse

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "./styles.css";
 
 import Image from "next/image";
+import Link from "next/link";
 import logo from "../../public/logo.png";
 import me from "../../public/Lorenzo.png";
 
@@ -15,7 +16,7 @@ export default function Home() {
       <header className="header-home">
         <div className="initial">
           <Image alt="História e Fé Católica logo" src={logo} width={25} />
-          <p>História e Fé Católica</p>
+          <p className="header-title">História e Fé Católica</p>
         </div>
         <DropdownMenu />
       </header>
@@ -31,7 +32,7 @@ export default function Home() {
             width="350px"
             height="190px"
             src="https://www.youtube.com/embed/6dE4LGxgL0Y"
-            title="YouTube video player"
+            title="História e Fé Católica Lorenzo Lazarotto"
             // frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             // allowfullscreen
@@ -39,19 +40,56 @@ export default function Home() {
         </div>
       </main>
       <section className="section-whoAmI">
-        <h2 className="title-whoAmI">
-          Quem é Lorenzo Lazzarotto:
-        </h2>
+        <h2 className="title-whoAmI">Quem é Lorenzo Lazzarotto:</h2>
         <div className="container-whoAmI">
           <div className="image-whoAmI">
             <figure className="src-whoAmI figure">
-              <Image src={me} alt="Lorenzo Lazzarotto" className="src-whoAmI link"/>
+              <Image
+                src={me}
+                alt="Lorenzo Lazzarotto"
+                className="src-whoAmI link"
+              />
             </figure>
           </div>
           <div className="description-whoAmI">
             <p className="paragraph-whoAmI">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam quis quos, praesentium nihil voluptatem ex. Maiores totam ipsam ut alias omnis reprehenderit, dolore doloremque voluptas eveniet tempora velit suscipit praesentium.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+              quis quos, praesentium nihil voluptatem ex. Maiores totam ipsam ut
+              alias omnis reprehenderit, dolore doloremque voluptas eveniet
+              tempora velit suscipit praesentium.
             </p>
+          </div>
+        </div>
+      </section>
+      <section className="section-endorse">
+        <h2 className="title-endorse">Apoie este Apostolado!</h2>
+        <div className="container-endorse">
+          <div className="description-endorse">
+            <p className="paragraph-endorse">
+              Desde 2020 decidi propagar a história e fé Católica para melhor
+              servir a Deus e sem interesses financeiros. Porém desde Março de
+              2023 tenho que arcar com o financiamento da minha faculdade junto
+              das despesas básicas.
+            </p>
+            <p className="paragraph-endorse">
+              Uma doação mensal, ainda que com valores baixos que não impactam
+              em sua vida financeira, fará TODA a diferença para mim. Assim,
+              poderei continuar a me dedicar em tempo integral ao Apostolado.
+            </p>
+          </div>
+          <div className="cta-endorse">
+            
+            <Link
+              href={
+                "https://www.kickante.com.br/crowdfunding/sua-ajuda-sustenta-esse-apostolado"
+              }
+              className="cta-link"
+              target="_blank"
+            >
+              <button className="button-endorse">
+                Quero ajudar este Apostolado!
+              </button>
+            </Link>
           </div>
         </div>
       </section>
--- my-app/src/components/DropDown/index.tsx
+++ my-app/src/components/DropDown/index.tsx
@@ -28,45 +28,15 @@ const DropdownMenu = () => {
 
   return (
     <div className="menu">
-      {/* <button
-        className="drop-button"
-        onClick={toggleDropdown}
-      >
-        <Image src={menuDrop} alt="" width={25} height={25} />
-      </button>
-      {isOpen && (
-        <ul className="absolute bg-white text-gray-800 pt-2 shadow-md menu-itens">
-          <li className="list-bullet">
-            <a href="#" className="block px-4 py-2 hover:bg-gray-200 link-item">
-              Apoie este apostolado
-            </a>
-            <hr />
-          </li>
-          <li className="list-bullet">
-            <a href="#" className="block px-4 py-2 hover:bg-gray-200 link-item">
-              Meu Livro
-            </a>
-            <hr />
-          </li>
-          <li className="list-bullet">
-            <a href="#" className="block px-4 py-2 hover:bg-gray-200 link-item">
-              Redes sociais
-            </a>
-            <hr />
-          </li>
-        </ul>
-      )} */}
       <Menu>
         <MenuButton
           as={IconButton}
-          // rightIcon={<ChevronDownIcon />}
           icon={<GiHamburgerMenu className="iconMenu"/>}
           background={"inherit"}
           width={40}
           height={40}
           border={"none"}
-        >
-        </MenuButton>
+        />
         <MenuList height={200}>
           <MenuItem  height={"20%"} width={180}>Apoie este apostolado</MenuItem>
           <MenuItem height={"20%"} width={180}>Meu Livro</MenuItem>
